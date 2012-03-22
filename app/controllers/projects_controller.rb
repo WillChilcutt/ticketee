de72f0cf52995cbb62ec033cedf19a1295664f58
Will <will@will-Aspire-5250.(none)>
2012-03-22 12:57:38 -0400

    Added title functionality for show page

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -10,7 +10,7 @@ class ProjectsController < ApplicationController
   def create
     @project = Project.new(params[:project])
     @project.save
-    flash[:note] = "Project has been created."
+    flash[:notice] = "Project has been created."
     redirect_to @project
   end
 
